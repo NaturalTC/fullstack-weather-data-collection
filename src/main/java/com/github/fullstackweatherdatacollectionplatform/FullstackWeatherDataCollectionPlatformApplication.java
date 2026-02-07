7b91d14a73e3cbe92b7ca1bf229054d383352cee
Jose <jose119999@gmail.com>
2026-02-06 23:11:04 -0500

    initial commit

---
+++ src/main/java/com/github/fullstackweatherdatacollectionplatform/FullstackWeatherDataCollectionPlatformApplication.java
@@ -0,0 +1,13 @@
+package com.github.fullstackweatherdatacollectionplatform;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FullstackWeatherDataCollectionPlatformApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FullstackWeatherDataCollectionPlatformApplication.class, args);
+    }
+
+}
