a10faef0d930736b9ceb1602f642312ac49eef40
Jose <jose119999@gmail.com>
2026-02-06 23:11:04 -0500

    initial commit

---
+++ src/test/java/com/github/fullstackweatherdatacollectionplatform/FullstackWeatherDataCollectionPlatformApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.fullstackweatherdatacollectionplatform;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FullstackWeatherDataCollectionPlatformApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
