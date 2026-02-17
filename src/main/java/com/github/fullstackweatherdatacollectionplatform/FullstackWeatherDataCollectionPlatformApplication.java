d73f52aee986be9cb4d77ab63dc823fa8ec8a4c8
Jose <jose119999@gmail.com>
2026-02-17 18:03:03 -0500

    swap to MySQL

--- src/main/java/com/github/fullstackweatherdatacollectionplatform/FullstackWeatherDataCollectionPlatformApplication.java
+++ src/main/java/com/github/fullstackweatherdatacollectionplatform/FullstackWeatherDataCollectionPlatformApplication.java
@@ -2,12 +2,19 @@ package com.github.fullstackweatherdatacollectionplatform;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.client.RestClient;
 
 @SpringBootApplication
 public class FullstackWeatherDataCollectionPlatformApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(FullstackWeatherDataCollectionPlatformApplication.class, args);
+    }
+
+    @Bean
+    public RestClient.Builder restClientBuilder() {
+        return RestClient.builder();
     }
 
 }
