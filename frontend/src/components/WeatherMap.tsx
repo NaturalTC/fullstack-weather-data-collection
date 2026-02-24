aea36cdf93a382e75fe5263e6f165605fdf50182
Jose <jose119999@gmail.com>
2026-02-23 23:06:34 -0500

    switch map to light basemap

--- frontend/src/components/WeatherMap.tsx
+++ frontend/src/components/WeatherMap.tsx
@@ -13,7 +13,7 @@ export default function WeatherMap() {
       scrollWheelZoom={false}
     >
       <TileLayer
-        url="https://{s}.basemaps.cartocdn.com/dark_all/{z}/{x}/{y}{r}.png"
+        url="https://{s}.basemaps.cartocdn.com/light_all/{z}/{x}/{y}{r}.png"
         attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> &copy; <a href="https://carto.com/">CARTO</a>'
       />
       <TileLayer
